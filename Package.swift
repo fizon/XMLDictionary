5c03e350c7f52d38d87b4275811dfe28e1fb4198
Jeremy Boy <github@jboy.eu>
2019-10-09 13:18:09 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "XMLDictionary",
+    products: [
+        .library(
+            name: "XMLDictionary",
+            targets: ["XMLDictionary"]),
+    ],
+    targets: [
+        .target(
+            name: "XMLDictionary",
+            dependencies: [],
+            path: "XMLDictionary",
+            publicHeadersPath: "XMLDictionary")
+    ]
+)
